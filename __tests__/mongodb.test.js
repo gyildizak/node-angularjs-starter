11307d982ca5a3235e03733011cebeb651dd853d
Mehmet Seven <mehmetseven0@gmail.com>
2018-06-26 23:53:04 +0300

    mongo tests

--- __tests__/mongodb.test.js
+++ __tests__/mongodb.test.js
@@ -1,7 +1,10 @@
-import db from '../helpers/db';
+import mongoose from "mongoose";
 
 describe('MongoDB Server', () => {
 	it('MongoDB server connection', done => {
-		done();
+		mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`)
+			.then(() => {
+				done()
+			});
 	});
 });
